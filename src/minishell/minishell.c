b3f1b73ff6c81577ca3d033652544a17bba4be0f
Nazrin shah Fernandez <nazrin2222@gmail.com>
2022-04-05 11:25:25 +0800

    handle signals for ctrl'\'

--- src/minishell/minishell.c
+++ src/minishell/minishell.c
@@ -6,31 +6,58 @@
 /*   By: hyun-zhe <hyun-zhe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 10:22:40 by hyun-zhe          #+#    #+#             */
-/*   Updated: 2022/04/04 15:41:54 by nfernand         ###   ########.fr       */
+/*   Updated: 2022/04/05 11:25:00 by nfernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-#define _SVID_SOURCE 1
+sig_atomic_t sigflag = 0;
 
-struct termios	termios_save;	
+struct termios	saved;
 
-sig_atomic_t	the_flag = 0;
-
-void	do_stuff(int num)
+void	reset_the_terminal()
 {
-	(void)num;
-	write(1, "\n", 1);
-	rl_replace_line("", 0);
-	rl_on_new_line();
-	rl_redisplay();
+	tcsetattr(0, 0,  &saved);
+}
+
+void	handle_signal(int num)
+{
+	if (num == SIGINT)
+	{
+		write(1, "\n", 1);
+		rl_replace_line("", 0);
+		rl_on_new_line();
+		rl_redisplay();
+		sigflag = 2;
+	}
+	else if (num == SIGQUIT)
+	{
+		write(1, "minishell % ", ft_strlen("minishell % "));
+		write(1, rl_line_buffer, ft_strlen(rl_line_buffer));
+		sigflag = 3;
+	}
+}
+
+void	read_signals()
+{
+	signal(SIGINT, handle_signal);
+	signal(SIGQUIT, handle_signal);
 }
 
 int	minishell(t_data *data)
 {
 	char	*line;
-	signal(2, do_stuff);
+	struct	termios	new;
+
+	tcgetattr(0 , &saved);
+	atexit(reset_the_terminal); //change this later illegal funcitno ujst reset the terminal normall on exit
+
+	new = saved;
+	new.c_lflag &= ~ECHOCTL;
+	tcsetattr(0, 0, &new);
+	read_signals();
+
 	line = readline("minishell % ");
 	while (line != NULL)
 	{
