e69a776ab132e426efacb85c4f948001c95450e9
Hoo Yun zhe <yunzhescientist@gmail.com>
2022-05-09 17:43:28 +0800

    fixed termios resetting during child process

--- src/minishell/minishell_utils.c
+++ src/minishell/minishell_utils.c
@@ -6,7 +6,7 @@
 /*   By: hyun-zhe <hyun-zhe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 10:33:50 by hyun-zhe          #+#    #+#             */
-/*   Updated: 2022/05/09 17:01:50 by hyun-zhe         ###   ########.fr       */
+/*   Updated: 2022/05/09 17:39:13 by hyun-zhe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,11 @@ void	ft_freestrs(char **strs)
 
 void	ft_exit(t_data *data, int exit_num)
 {
-	tcsetattr(0, 0, &data->original_term);
 	free_data(data);
 	exit(exit_num);
 }
+
+/**
+	supposedly to reset the terminal to its original state but fuck it cause it breaks minishell instead
+	tcsetattr(0, 0, &data->original_term);
+**/
